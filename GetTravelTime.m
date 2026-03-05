10185b1d14f27a23c7f7c5ab31065195fbd98d7f
JessSun <18140663621@163.com>
2026-03-05 22:29:31 +0800

    Update GetTravelTime.m

--- GetTravelTime.m
+++ GetTravelTime.m
@@ -1,4 +1,4 @@
-function [TxTime,satClkErr, pseudoRange] = GetTravelTime(RxTime,RxPos,eph,settings)
+function [TxTime,satClkErr,IonoErr, pseudoRange] = GetTravelTime(RxTime,RxPos,eph,ionoutc,settings)
 % Get signal transmitting time and travel time.
 %
 %[Txtime,travelTime] = GetTravelTime(RxTime,RxPosEcef,eph,settings)
@@ -8,6 +8,7 @@ function [TxTime,satClkErr, pseudoRange] = GetTravelTime(RxTime,RxPos,eph,settin
 %       RxPos             - Receiver position in ECEF coordinate.
 %       eph               - Ephemeris.
 %       settings          - simulator settings.
+%       ionoutc           - Ionospheric Correction Parameters.
 %   Outputs:
 %       Txtime            - Signal transmitting time.
 %       travelTime        - Signal travel time.
@@ -27,6 +28,7 @@ TxTime = zeros(1,length(RxTime));
 travelTime = zeros(1,length(RxTime));
 % Satellite clock error including relativistic effect and tgd
 satClkErr = zeros(1,length(RxTime));
+IonoErr = zeros(1,length(RxTime));
 pseudoRange = zeros(1,length(RxTime));
 
 for index = 1:length(RxTime)
@@ -53,19 +55,26 @@ for index = 1:length(RxTime)
         Rot_X = e_r_corr(traveltimeTemp, satPos);
         % Corrected transmitting time
         traveltimeTemp = norm(Rot_X - RxPosTemp) / settings.c;
-        TxtimeCorrect = RxTime(index) - traveltimeTemp;
+        TxtimeCorrect1 = RxTime(index) - traveltimeTemp;
+
+        % Calculate ionospheric delay
+        ionodel(index) = ionocorr(TxtimeCorrect1,satPos, RxPosTemp,ionoutc);
+
+        TxtimeCorrect2 = RxTime(index) - traveltimeTemp - ionodel(index)/settings.c;
         
         % Finish iteration for transmitting time search
-        if abs(TxtimeCorrect - TxTimeTemp) < threshold
-            TxTime(index) = TxtimeCorrect;
+        if abs(TxtimeCorrect2 - TxTimeTemp) < threshold
+            TxTime(index) = TxtimeCorrect2;
             travelTime(index) = traveltimeTemp;
-            satClkErr(index) = GpsClockCorrection(TxtimeCorrect,eph) + relaError;
+            satClkErr(index) = GpsClockCorrection(TxtimeCorrect2,eph) + relaError;
+            IonoErr(index) = ionodel(index);
+
             % Calculate the pseudorange for each satellite
-            pseudoRange(index) = travelTime(index) * settings.c + satClkErr(index) * settings.c;
+            pseudoRange(index) = travelTime(index) * settings.c + satClkErr(index) * settings.c + IonoErr(index);
             break
         end
         % For next interation
-        TxTimeTemp = TxtimeCorrect;
+        TxTimeTemp = TxtimeCorrect2;
     end   % iterCnt = 1:10 
 end   % index = 1:length(RxTime)
 
